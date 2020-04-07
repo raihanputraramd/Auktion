482198f063fddea3f50f83e0fed9ba6af3b2097a
Putra <raihanputraramd@gmail.com>
2020-04-07 09:44:13 +0700

    finishing layout admin

---
+++ app/src/main/java/com/example/auktion/adminActivity/AdminDetailItemActivity.java
@@ -0,0 +1,14 @@
+package com.example.auktion;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdminDetailItemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_detail_item);
+    }
+}
